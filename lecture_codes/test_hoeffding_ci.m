77ebf57ef3ad312ce463689b4632186742d43b43
Ayush Kumar Shah <ayush.kumar.shah@gmail.com>
2020-11-11 01:23:56 +0545

    feat: Change to fraction missed

--- lecture_codes/test_hoeffding_ci.m
+++ lecture_codes/test_hoeffding_ci.m
@@ -1,6 +1,6 @@
 function test_hoeffding_ci
 
-Ns = [10 100 1000];
+Ns = [10 100 1000 10000];
 alphas = [0.25 0.05];
 fraction = zeros(length(alphas), length(Ns));
 eps = zeros(length(alphas), length(Ns));
@@ -10,8 +10,8 @@ for i=1:length(alphas)
     for j=1:length(Ns)    
         N = Ns(j);
         [fraction(i, j), eps(i, j)] = get_frac_hoeffding_ci(N, alpha);       
-        fprintf('alpha: %1.3f\t N: %5d\t epsilon: %1.3f\t fraction: %1.3f\n', ...
-            alpha, N, eps(i, j), fraction(i, j));                            
+        fprintf('alpha: %1.2f\t N: %5d\t epsilon: %1.3f\t fraction missed: %1.3f\n', ...
+            alpha, N, eps(i, j), 1 - fraction(i, j));                            
     end
     fprintf('\n');
 end
